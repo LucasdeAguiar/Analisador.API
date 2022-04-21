728fa746386acf7cd6eeff3bbca591615b80b21f
LucasdeAguiar <80419489+LucasdeAguiar@users.noreply.github.com>
2022-04-20 22:10:52 -0300

    Add project files.

---
+++ Compilador.API/Compilador.API/Controllers/Compilador.cs
@@ -0,0 +1,12 @@
+﻿namespace Compilador.API.Controllers
+{
+    public class Compilador
+    {
+        public string mensagem = "Token: ";
+
+        public void compilar(string mensagemObjeto)
+        {
+            this.mensagem = this.mensagem + mensagemObjeto + "abcde";
+        }
+    }
+}
